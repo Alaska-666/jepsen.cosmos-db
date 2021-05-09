31ad9e605b43b0f0c1c3a3d9936d90c0b79dde26
isaeva anna <pangolin-666@mail.ru>
2021-05-09 22:35:17 +0300

    fixed

--- src/jepsen/cosmosDB/db.clj
+++ src/jepsen/cosmosDB/db.clj
@@ -1,19 +1,19 @@
 (ns jepsen.cosmosDB.db
   (:require [clojure.tools.logging :refer :all]
-            [clojure.string :as str]
-            [jepsen [cli :as cli]
-             [control :as c]
-             [db :as db]
-             [tests :as tests]]
-            [jepsen.control.util :as cu]))
+            [jepsen [db :as db]]))
 
+(def dir     "/opt/cosmosDB")
+(def logfile (str dir "/cosmosDB.log"))
 
 (defn db
   "???"
-  [opts]
   (reify db/DB
     (setup! [_ test node]
-      (info node "setup cosmos db"))
+      (info node "setup! cosmos db"))
 
     (teardown! [_ test node]
-      (info node "tearing down cosmos db"))))
+      (info node "tearing down cosmos db"))
+
+    db/LogFiles
+    (log-files [_ test node]
+      [logfile])))
