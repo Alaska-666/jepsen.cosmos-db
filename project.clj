d30a43d144a2d246b805db2993d95257a9326839
isaeva anna <pangolin-666@mail.ru>
2021-05-09 21:08:34 +0300

    scaffolding

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject cosmos-db-jepsen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Jepsen test for Cosmos db"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main cosmos-db-jepsen
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [jepsen "0.2.1-SNAPSHOT"]
+                 [com.azure/azure-cosmos "4.0.0-preview.1"]]
   :repl-options {:init-ns cosmos-db-jepsen.core})
--- src/cosmos_db_jepsen/core.clj
+++ src/cosmos_db_jepsen/core.clj
@@ -1,6 +1,31 @@
-(ns cosmos-db-jepsen.core)
+(ns cosmos-db-jepsen.core
+  (:require [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;(defn -main
+;  "Handles command line arguments. Can either run a test, or a web server for
+;  browsing results."
+;  [& args]
+;  (cli/run! (merge (cli/single-test-cmd {:test-fn  mongodb-test
+;                                         :opt-spec cli-opts})
+;                   (cli/test-all-cmd {:tests-fn (partial all-tests mongodb-test)
+;                                      :opt-spec cli-opts})
+;                   (cli/serve-cmd))
+;            args))
+
+
+(defn cosmosdb-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true}
+         opts))
+
+(defn -main
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
+  [& args]
+  (cli/run! (merge (cli/single-test-cmd {:test-fn cosmosdb-test})
+                   (cli/serve-cmd))
+            args))
