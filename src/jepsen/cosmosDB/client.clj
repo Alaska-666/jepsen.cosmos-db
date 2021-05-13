92d9cf346ab816ab497c265ee340309998745d7e
isaeva anna <pangolin-666@mail.ru>
2021-05-13 21:28:59 +0300

    fixed client

--- src/jepsen/cosmosDB/client.clj
+++ src/jepsen/cosmosDB/client.clj
@@ -7,8 +7,7 @@
   (:import (com.azure.cosmos CosmosClientBuilder
                              CosmosClient
                              ConsistencyLevel
-                             CosmosDatabase
-                             CosmosDatabaseResponse)))
+                             CosmosDatabase)))
 
 
 (defn ^CosmosClient build-client
