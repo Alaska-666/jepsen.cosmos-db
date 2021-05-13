37f7bf1ebb125e1226070b48fea88ee09cb26311
isaeva anna <pangolin-666@mail.ru>
2021-05-13 23:46:28 +0300

    fixed client

--- src/jepsen/cosmosDB/listAppend.clj
+++ src/jepsen/cosmosDB/listAppend.clj
@@ -3,7 +3,6 @@
   (:require [clojure.tools.logging :refer :all]
             [clojure [string :as str]
              [pprint :refer [pprint]]]
-            [dom-top.core :refer [with-retry]]
             [jepsen [client :as client]]
             [jepsen.cosmosDB [client :as c]])
   (:import (com.azure.cosmos CosmosClientBuilder
@@ -18,24 +17,22 @@
 (defrecord Client [conn database container account-host account-key consistency-level]
   client/Client
   (open! [this test node]
-    (assoc this
-      :conn (c/build-client node account-host account-key consistency-level))
-    )
-
-  (setup! [this test]
-    (let [database  (c/createDatabaseIfNotExists conn databaseName)]
+    (let [conn      (c/build-client node account-host account-key consistency-level)
+          database  (c/createDatabaseIfNotExists conn databaseName)]
       (assoc this
+        :conn       conn
         :database   database
         :container  (c/createContainerIfNotExists database containerName throughput partitionKeyPath))
       )
     )
+
+  (setup! [this test])
 
   (invoke! [_ test op])
 
   (teardown! [this test])
 
   (close! [_ test]
-      (.delete container)
-      (.delete database)
-    )
+    (.delete container)
+    (.delete database))
   )
